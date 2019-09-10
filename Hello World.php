9f5b3ff7d8b0805c1a7d05bbfa86166ae5d921c9
NaitamG <46876318+NaitamG@users.noreply.github.com>
2019-09-10 12:06:23 -0400

    Delete Hello World.php

--- Hello World.php
+++
@@ -1,4 +0,0 @@
-
-<?php    
-echo "Hello World";                                                                                                                            echo "Hello World";
-?>Connection reset by 128.235.211.21 port 22
