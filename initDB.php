01a0e477e20048b0a5debff6cc6449e0ac5eb635
NaitamG <naitam@ymail.com>
2019-09-18 11:48:34 -0400

    adding initDB.php

--- initDB.php
+++ initDB.php
@@ -6,15 +6,13 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require('config.php');
-echo "Loaded Host: " . $host;
-<<<<<<< HEAD
+echo $host;
+
 $conn_string = "mysql:host=$host;dbname=$database;usename=$username;password=$password;charset=utf8mb4";
-=======
-$conn_string = "mysql:host=$host;dbname=$database;username=$username;password=$password;charset=utf8mb4";
->>>>>>> e69c68127e3839609828519a116271b60f9c1428
+
 try{
 	$db = new PDO($conn_string, $username, $password);
-	echo "Connected";
+	echo "Should have Connected";
 }
 catch(Exception $e){
 	echo $e->getMessage();
