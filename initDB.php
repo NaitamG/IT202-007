662c6264b8b94236e5b536118490146d41f3fdc2
NaitamG <naitam@ymail.com>
2019-09-18 12:23:11 -0400

    adding initDB.php

--- initDB.php
+++ initDB.php
@@ -13,6 +13,17 @@ $conn_string = "mysql:host=$host;dbname=$database;usename=$username;password=$pa
 try{
 	$db = new PDO($conn_string, $username, $password);
 	echo "  Connected";
+	// Create Table
+	$query = "create table if not exists `TestUsers`(
+		`id` int auto_increment not null,
+		`username` varchar(30) not null unique,
+		`pin` int default 0,
+		PRIMARY KEY (`id`)
+		) CHARACTER SET utf8 COLLATE utf8_general_ci";
+	$stmt = $db->prepare($query);
+	$r = $stmt->execute();
+	echo "<br>" . ($r>0?"Created table or already exists":"Failed to create table") . "<br>";
+	unset($r);
 }
 catch(Exception $e){
 	echo $e->getMessage();
