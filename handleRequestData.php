21b044ae6e9943ba4d24d7eb09f710ae0bf0020e
NaitamG <naitam@ymail.com>
2019-09-25 12:03:34 -0400

    adding handleRequestData.php

--- handleRequestData.php
+++ handleRequestData.php
@@ -1,8 +1,8 @@
-<? php
+<?php
 	echo "<pre>" . var_dump($_GET, true) . "</pre";
 	
-	if(isset($_GET['name'])){
-		echo "<br>Hello, " . $_Get['name'] . "<br>";
+	if(isset($_GET[`name`])){
+		echo "<br>Hello, " . $_Get[`name`] . "<br>";
 	}
 	//web.njit.edu/~ng395/IT202-007/handleRequestData.php?parameter1=a&p2=b
 >?
