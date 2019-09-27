d8ca770f4c967c7f5c1a9d6eb6f0433f3bfc7fb7
NaitamG <naitam@ymail.com>
2019-09-27 14:23:15 -0400

    addding handleRequestData.php

--- handleRequestData.php
+++ handleRequestData.php
@@ -1,8 +1,22 @@
 <?php
 	echo "<pre>" . var_dump($_GET, true) . "</pre";
 	
-	if(isset($_GET[`name`])){
-		echo "<br>Hello, " . $_Get[`name`] . "<br>";
+  if(isset($_GET['name'])){
+		echo "<br>Hello, " . $_Get['name'] . "<br>";
 	}
-	//web.njit.edu/~ng395/IT202-007/handleRequestData.php?parameter1=a&p2=b
->?
+  if(isset($_GET['num1', 'num2'])){
+		echo "<br>" . $_GET['num1' + 'num2'] . " should be a number...";
+		echo <br>"but it might not be"<br>;
+	}
+  else {
+     echo <br>"write a number."<br>;
+  }
+  if(isset($_GET[] 
+	
+//TODO
+//handle addition of two or more parametes with proper number parsing
+//concatenate 2 or more parameter values and echo them
+//try passing multiple same-named parametes with different values
+//try passing a parameter value with special characters
+//web.njit.edu/~ng395/IT202-007/handleRequestData.php?parameter1=a&p2=b
+?>
