960417e9550f017a7e3eb23c97c92ae0d58a5866
NaitamG <naitam@ymail.com>
2019-09-29 19:34:24 -0400

    adding handleRequestData.php

--- handleRequestData.php
+++ handleRequestData.php
@@ -1,25 +1,22 @@
 <?php
-
-  $something = $_GET['age'];
-  echo $something;
-/*	echo "<pre>" . var_dump($_GET, true) . "</pre";
-	
-  if(isset($_GET['name'])){
-		echo "<br>Hello, " . $_Get['name'] . "<br>";
-	}*/
-  /*
-  if(isset($_GET['num1', 'num2'])){
-		echo "<br>" . $_GET['num1' + 'num2'] . " should be a number...";
-		echo <br>"but it might not be"<br>;
+	echo "<pre>" . var_export($_GET, true) . "</pre>";
+	if(isset($_GET['name'])){
+		echo "<br>Hello, " . $_GET['name'] . "<br>";
 	}
-  else {
-     echo <br>"write a number."<br>;
-  }*/
-	
-//TODO
-//handle addition of two or more parametes with proper number parsing
-//concatenate 2 or more parameter values and echo them
-//try passing multiple same-named parametes with different values
-//try passing a parameter value with special characters
-//web.njit.edu/~ng395/IT202-007/handleRequestData.php?parameter1=a&p2=b
+  
+	if(isset($_GET['number'])){
+		$number = $_GET['number'];
+		echo "<br>Make " . $number . " an interger please";
+	} 
+ 
+ 
+  echo "<br>Having two paramters with same name results in the latest value displayed.<br>";
+ 
+  echo "<br>Having special characters as paramters results a string of them being displayed.<br>";
+	//TODO
+	//handle addition of 2 or more parameters with proper number parsing
+	//concatenate 2 or more parameter values and echo them
+	//try passing multiple same-named parameters with different values
+	//try passing a parameter value with special characters
+	//web.njit.edu/~ucid/IT202/handleRequestData.php?parameter1=a&p2=b
 ?>
