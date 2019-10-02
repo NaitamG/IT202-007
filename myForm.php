2c7386f4358edcb39d572ad3eb9999a0dffb165f
NaitamG <naitam@ymail.com>
2019-10-02 12:31:14 -0400

    adding myForm.php

--- myForm.php
+++ myForm.php
@@ -9,12 +9,23 @@ fuciton getName({
 <html>
 <head></head>
 <body>
-<?php getName();?.
+<?php getName();?>
 <form mode="GET" action="#">
-<input name="name" tpye="type" placeholder="Enter you name"/>
+<input name="name" tpye="text" placeholder="Enter you name"/>
+<input name="number" tpye="text" placeholder="Enter you number"/>
+<input name="password" type="password" placeholder="Enter a FAKE password"/>
+<input type="radio" name="radio" value="Yes"/>
+<input type="radio" name="radio" value="No"/>
+<input type="checkbox" values="check"/>
+<textarea name="text'></textarea>
+<inut type="data" name="date"/>
 <input type="submit" values="Try it"/>
 </form>
 </body>
 </html>
 
+<?php
+if(isset($_GET)){
+	echo "<br><pre>" . var_export($_GET, true) . "</pre><br>";
+}
 		
