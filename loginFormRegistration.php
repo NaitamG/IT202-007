c5f44429675cbc68fa17aa465e3e027128dc162f
NaitamG <naitam@ymail.com>
2019-10-31 22:55:30 -0400

    adding loginFormRegistration.php

--- loginFormRegistration.php
+++ loginFormRegistration.php
@@ -21,24 +21,25 @@
 </html>
 
 <?php
-require('config.php');
-$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
-$db = new PDO($conn_string, $username, $password);
-$select_query = " select * from `LoginForm` where `Username` = ':username' and `Password` = ':password'"
-$stmt = $db->prepare($select_query);
-$r = $stmt->execute(array(":username"=> "$user"));
-$results = $stmt->fetch(PDO::FETCH_ASSOC);
-echo "<pre>" . var_export($results, true) . "</pre>"; 
-$rows = mysqli_num_rows($results);
-
+//$r = $stmt->execute(array(":username"=> "$user"));
+//echo "<pre>" . var_export($results, true) . "</pre>"; 
 $user = $_POST['username'];
 $pass = $_POST['password'];
+  
+if(isset($_POST['username']) && isset($_POST['password'])){
+  require('config.php');
+  $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+  $db = new PDO($conn_string, $username, $password);
+  $select_query = " select * from `LoginForm` where `Username` = ':username' and `Password` = ':password'"
+  $stmt = $db->prepare($select_query);
+  $r = $stmt->execute(array(":username"=> "$user"));
+  $results = $stmt->fetch(PDO::FETCH_ASSOC);
+  echo "<pre>" . var_export($results, true) . "</pre>"; 
+  //$results = mysqli_query($conn_string, $select_query);
+  $row = mysqli_num_rows($results);
 
-if(isset($_POST['username']) && isset($_POST['password']){
-
-  if($row['username'] == $user && $row['password'] == $pass ){
+  if($row['username'] == $user && $_POST['password'] == $pass){ 
     echo "Login success!! Welcome";
-    exit();
   }
   else {
     echo "Unable to Login, Invalid Username and/or Password";
