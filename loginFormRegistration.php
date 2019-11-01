b2ed4acc02e5f163ae72b3eee333a697ca74a438
NaitamG <naitam@ymail.com>
2019-10-31 22:07:58 -0400

    adding loginFormRegistration.php

--- loginFormRegistration.php
+++ loginFormRegistration.php
@@ -22,9 +22,6 @@
 
 <?php
 require('config.php');
-$user = $_POST['username'];
-$pass = $_POST['password'];
-
 $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 $db = new PDO($conn_string, $username, $password);
 $select_query = " select * from `LoginForm` where `Username` = ':username' and `Password` = ':password'"
@@ -34,9 +31,12 @@ $results = $stmt->fetch(PDO::FETCH_ASSOC);
 echo "<pre>" . var_export($results, true) . "</pre>"; 
 $rows = mysqli_num_rows($results);
 
+$user = $_POST['username'];
+$pass = $_POST['password'];
+
 if(isset($_POST['username']) && isset($_POST['password']){
 
-  if($row['username'] == $use && $rrow['password'] == $pass ){
+  if($row['username'] == $user && $row['password'] == $pass ){
     echo "Login success!! Welcome";
     exit();
   }
