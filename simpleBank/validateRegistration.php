cc3239e45821004c81e85ef9955f95c2935fa476
NaitamG <naitam@ymail.com>
2019-11-20 12:20:24 -0500

    adding changed

---
+++ simpleBank/validateRegistration.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+function checkPasswords(){
+	if(isset($_POST['password']) && isset($_POST['confirm'])){
+		if($_POST['password'] == $_POST['confirm']){
+			echo "<br>Passwords Matched!<br>";
+		}
+		else{
+			echo "<br>Passwords didn't match!<br>";
+		}
+	}
+}
+?>
+
+<?php checkPasswords();?>
+
+<?php
+if(isset($_POST)){
+	echo "<br><pre>" . var_export($_POST, true) . "</pre><br>";
+}
+?>
